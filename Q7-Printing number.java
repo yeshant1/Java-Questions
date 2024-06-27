44e05162742089745e3e831a114095b4abfcaf6a
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-06-27 20:44:51 +0530

    Create Q7-Printing number.java

---
+++ Q7-Printing number.java
@@ -0,0 +1,45 @@
+You go to your relative's house with your parents and relatives to check your skills by giving a coding problem. You have to solve the given problem to show you are intelligent. They give you two integers A and B and ask to print prime numbers between A and B (inclusive).
+
+Input Format
+
+First line contains the T- number of test cases. Second line contains A and B.
+
+Constraints
+
+0<=T<=200; 0<=A <=B<=10000
+
+Output Format
+
+Print prime number between A and B.
+
+
+/*
+      Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt(); // Number of test cases
+
+        while (T-- > 0) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            for (int i = A; i <= B; i++) {
+                if (isPrime(i)) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;*/
