c493c0609bdda1c12d3a1f7c0cf7dbf4a75a4f2e
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-06-29 20:21:05 +0530

    Create Q11-Java Substring Comparisons.java

---
+++ Q11-Java Substring Comparisons.java
@@ -0,0 +1,62 @@
+We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string,s , and an integer,k , complete the function so that it finds the lexicographically smallest and largest substrings of length k .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting s .
+The second line contains an integer denoting k.
+
+Constraints  1<= |s| <= 1000
+
+s consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+
+/*public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i <= s.length() - k; i++) {
+            String substring = s.substring(i, i + k);
+            if (substring.compareTo(smallest) < 0) {
+                smallest = substring;
+            }
+            if (substring.compareTo(largest) > 0) {
+                largest = substring;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        String s = "welcometojava";
+        int k = 3;
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
+*/
