2130466d601328ca8936c3a486e229396a9d1e83
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-07 21:54:21 +0530

    Update Q23-Make An Array.java

--- Q23-Make An Array.java
+++ Q23-Make An Array.java
@@ -1,4 +1,36 @@
- Scanner scanner = new Scanner(System.in);
+You are given number n. Make an array assuming the index starts from 1. Array of i is Cipher if i is divisible by 3 and 5. Array of i is Schools if i is divisible by 3. Array of i is CipherSchools if the above condition does not match.
+
+Input Format
+
+First line of input contains T- number of test cases. Each test case contains n.
+
+Constraints
+
+1<=T<=100, 1<=n<=1000
+
+Output Format
+
+Print the array of string.
+
+Sample Input 0
+
+3
+3
+5
+8
+Sample Output 0
+
+CipherSchools CipherSchools Schools 
+CipherSchools CipherSchools Schools CipherSchools CipherSchools 
+CipherSchools CipherSchools Schools CipherSchools CipherSchools Schools CipherSchools CipherSchools
+ /*import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scanner = new Scanner(System.in);
 
         // Read the number of test cases
         int T = scanner.nextInt();
@@ -35,3 +67,5 @@
         }
 
         return result;
+    }
+}*/
