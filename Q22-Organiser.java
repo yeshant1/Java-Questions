b64006747cf8ca77a75c165d1f4cce869edd5acf
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-07 21:50:42 +0530

    Create Q22-Organiser.java

---
+++ Q22-Organiser.java
@@ -0,0 +1,29 @@
+Your friend is the organiser of an event. In this event they have to give 10 problems of rating A to L. They run the event for m round. They have a question bank of n problems and you have to tell how many extra problems he needs to organise m round event.
+
+Input Format
+
+First line of input contains T- number of test cases. Each test case contains two integers n and m. In the next line given string.
+
+Constraints
+
+1<=T<=50, 1<=n,m<=100
+
+Output Format
+
+Print single integer.
+
+Sample Input 0
+
+3
+10 1
+BGECDCBDED
+10 2
+BGECDCBDED
+9 1
+BBCDEFFGG
+Sample Output 0
+
+7
+15
+6
+  
