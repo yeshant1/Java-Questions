cb52afc8843f07a4a08db9ec73f6e15247d8f342
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-09 21:44:19 +0530

    Create Q28-2D.java

---
+++ Q28-2D.java
@@ -0,0 +1,26 @@
+You are given a two dimensional array and each element of the array represents the pixel of the photo. Your task reverses each row of a two dimensional array.
+
+Input Format
+
+first line of input contains T-number of test cases. Each test case contains n size of array. The next line contains an array.
+
+Constraints
+
+1<=T<=100, 1<=n<=100
+
+Output Format
+
+Print new array.
+
+Sample Input 0
+
+1
+3
+1 1 0
+1 0 1
+0 0 0
+Sample Output 0
+
+1 0 0 
+0 1 0 
+1 1 1
