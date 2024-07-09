10fc7d5a9df8f6d457771d7a407e10d98b7d86f3
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-09 21:45:09 +0530

    Create Q29-Find m.java

---
+++ Q29-Find m.java
@@ -0,0 +1,25 @@
+You are given an array of integers and you are also given value k and you remove all the values from the array which is equal k. Now you have to tell what is the first m value in the array such that k does not appear.
+
+Input Format
+
+First line of input contains T- number of test cases. Each test case contains n and k size of array and value respectively. The next line contains an array.
+
+Constraints
+
+1<=T<=100, 1<=n<=1000
+
+Output Format
+
+Print m.
+
+Sample Input 0
+
+2
+4 3
+3 2 2 3
+8 2
+0 1 2 2 3 0 4 2
+Sample Output 0
+
+2
+5
