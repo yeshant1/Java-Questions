a1aa4cb157160e08a7fc52a3ecd99c87551e2525
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-10 21:50:20 +0530

    Create Q32-SortString.java

---
+++ Q32-SortString.java
@@ -0,0 +1,31 @@
+You are given a string that only g, h, i and your task is to sort this string by doing a given operation at max one time. Operation is to select two characters from a string and swap them. You have to tell whether it is possible to sort the string or Not. Print YES if it is possible otherwise No.
+
+Input Format
+
+First line of input contains T- number of test cases. Each test case contains a string.
+
+Constraints
+
+.
+
+Output Format
+
+Print YES or NO
+
+Sample Input 0
+
+6
+abc
+acb
+bac
+bca
+cab
+cba
+Sample Output 0
+
+YES
+YES
+YES
+NO
+NO
+YES
