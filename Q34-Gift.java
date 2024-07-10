2fef1ef54be5b94b12a4406d54a4f61c0702b5fc
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-10 22:01:28 +0530

    Create Q34-Gift.java

---
+++ Q34-Gift.java
@@ -0,0 +1,45 @@
+You and your friend go to the birthday party of your common friend Vika. Your friend buys a gift for vika that contains a cardboard of n cross m size each column has random letters. You know vika is like a gift if each column contains v, i, k, a. Otherwise he doesn't like gifts. You have to tell Yes if raja likes gifts otherwise No.
+
+Input Format
+
+First line of input contains T- number of test cases. Each Test case contains n and m. The next line contains a two dimensional character matrix.
+
+Constraints
+
+1<=T<=100, 1<=n,m<=100
+
+Output Format
+
+Print YES or NO.
+
+Sample Input 0
+
+5
+1 4
+vika
+3 3
+bad
+car
+pet
+4 4
+vvvv
+iiii
+kkkk
+aaaa
+4 4
+vkak
+iiai
+avvk
+viaa
+4 7
+vbickda
+vbickda
+vbickda
+vbickda
+Sample Output 0
+
+YES
+NO
+YES
+NO
+YES
