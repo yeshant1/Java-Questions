b505d8a742d60c80b180ff9f83edafab8e8b3e80
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-12 22:05:34 +0530

    Create Q40-ReachFriendHome1.java

---
+++ Q40-ReachFriendHome1.java
@@ -0,0 +1,25 @@
+You want to go to your friend home and you reach the entry gate of society but your friend’s society is very complex means society is look like grid of n row and m column and entry gate of society is 0th row and 0th column and each house at their number and your friend give their own house number x. You have to reach the house whose number is x but society is very complex then there is a possibility of not reaching your friend’s home. Now you have to say Yes or No. if you reach then print YES otherwise NO.
+
+Input Format
+
+First line of input contains T- number of test cases. Each test case contains n, m, target. The next line contains a 2D array.
+
+Constraints
+
+1<=T<=100, 1<=n,m<=100
+
+Output Format
+
+Print YES or NO.
+
+Sample Input 0
+
+5 5 30
+26    2    26    61    20    
+93    43    50    65    77    
+1    91    60    80    73    
+43    73    63    95    32    
+92    79    59    22    21
+Sample Output 0
+
+NO
