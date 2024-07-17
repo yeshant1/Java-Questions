e95b8ea5331339f04ddd796155a934336506caee
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-17 10:24:24 +0530

    Create Q52-LinkedListCycle.java

---
+++ Q52-LinkedListCycle.java
@@ -0,0 +1,21 @@
+//leetcode 141
+//leetcode 141
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null){
+            fast = fast.next;
+            if(fast != null){
+              fast = fast.next;
+            }
+           slow = slow.next;
+
+        if(slow == fast){
+         return true;
+        }
+     return false;
+  }
+ }
