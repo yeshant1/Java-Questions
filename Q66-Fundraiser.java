7e916c67b5adca22ee96e103e1130412cec6ca28
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-28 14:30:18 +0530

    Create Q66-Fundraiser.java

---
+++ Q66-Fundraiser.java
@@ -0,0 +1,25 @@
+Alice and Bob are organizing a charity fundraiser. They have a limited number of special donation coins to distribute to participants. There are two types of coins: gold coins worth 75 units each and silver coins worth 10 units each. Game Rules: The game starts with Alice, and they take turns picking up coins. On each turn, a player must pick coins with a total value of exactly 115 units. If a player cannot pick coins that total 115 units on their turn, they lose the game. Given the number of gold coins (x) and silver coins (y) they have, determine which player will win if both Alice and Bob play optimally.
+
+Input Format
+
+First line of input contains T number of test cases. Each test case contains two integers x and y.
+
+Constraints
+
+1<=T<=100, 1<=x,y<=100
+
+Output Format
+
+Print name of player who wins the game.
+
+Sample Input 0
+
+2
+4 11
+2 7
+Sample Output 0
+
+Bob
+Alice
+
+  
