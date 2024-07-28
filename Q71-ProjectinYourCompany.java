7546de229e4975d668066e42c919022f104fb443
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-28 14:53:57 +0530

    Create Q71-ProjectinYourCompany.java

---
+++ Q71-ProjectinYourCompany.java
@@ -0,0 +1,29 @@
+You and your friend are working on a project at your tech company, and you come across a problem that requires transforming one array into another. Here are the specifics of the problem: You are given two positive integer arrays nums and target, both of the same length. You need to transform nums into target using the following operation: In a single operation, you can select any subarray of nums and increment or decrement each element within that subarray by 1. Your task is to determine the minimum number of operations required to make nums equal to the array target.
+
+Input Format
+
+First line of input contains T- number of test cases. Each test case contains an integer n - size of array. The next line contains two array nums and target.
+
+Constraints
+
+1<=T<=100, 1<= n <= 10000
+
+Output Format
+
+Print minimum number of operations required to transform nums to target.
+
+Sample Input 0
+
+2
+4
+3 5 1 2
+4 6 2 4
+3
+1 3 2
+2 1 4
+Sample Output 0
+
+2
+5
+
+  
